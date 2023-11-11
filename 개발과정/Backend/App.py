7f5d35c83ba3f66a2bc30e31c7502bab58c04aca
JinCheol Shin <114928709+justgotothedesk@users.noreply.github.com>
2023-11-11 11:18:28 +0900

    Update App.py

--- 개발과정/Backend/App.py
+++ 개발과정/Backend/App.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+# text-bison을 사용하여 결과값을 반환하는 test.py 파일의 test Class 임포트
+from test import test
 
 app = Flask(__name__)
 CORS(app)
@@ -7,17 +9,20 @@ CORS(app)
 question_history = []
 
 @app.route('/', methods=['GET', 'POST'])
-def test():
+def start():
     if request.method == 'GET':
         return jsonify({"question_history": question_history})
 
     if request.method == 'POST':
         data = request.json
         question = data.get("question")
-        
         question_history.append(question)
+
+        chatbot = test()
+        answer = chatbot.service(question)
+
         
-        response_data = {"message": "Data received", "question": question}
+        response_data = {"question": question, "answer": answer}
         return jsonify(response_data)
 
 if __name__ == '__main__':
