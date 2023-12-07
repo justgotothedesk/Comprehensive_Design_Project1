f830faf0bc2d61aa54e6df0cca856f1190230e2d
mouse4432 <mouse4432@gmail.com>
2023-12-08 08:02:01 +0900

    sidebar and header init

--- 개발과정/Front/src/component/Sidebar.jsx
+++ 개발과정/Front/src/component/Sidebar.jsx
@@ -3,24 +3,15 @@ import './Sidebar.css';
 
 const Sidebar = () => {
   return (
-    <div className="sidebar-container">
-      <div className="chat-history">
-        
-        <ul>
-          <li><button>Chat 1</button></li>
-          <li><button>Chat 2</button></li>
-          {/* 추가적인 채팅 기록 버튼들 */}
-        </ul>
+    <div id="sidebar">
+      <div className="header-container">
+        <img src="logo.png" alt="logo" style={{ width: '100px', height: '100px' }} />
+        <h3 style={{ marginLeft: '10px' }}>환영합니다.</h3>
       </div>
-
-      <div className="setting">
-        <div>
-            <button className='buttons'>Help</button>
-        </div>
-        <div>
-            <button className='buttons'>Setting</button>
-        </div>
-      </div>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Contact</a>
     </div>
   );
 };
