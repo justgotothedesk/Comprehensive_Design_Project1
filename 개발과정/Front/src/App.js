201a5be13253e062c42bc9faa43dd6a098f653f6
mouse4432 <mouse4432@gmail.com>
2023-12-08 22:39:06 +0900

    setup for mobile & pc design

--- 개발과정/Front/src/App.js
+++ 개발과정/Front/src/App.js
@@ -1,15 +1,37 @@
+import React from 'react'
 import './App.css';
 import Main from './component/Main';
 import Header from './component/Header';
 import Sidebar from './component/Sidebar';
+import {useMediaQuery} from 'react-responsive';
 
+export const Mobile = ({children}) => {
+  const isMobile = useMediaQuery({
+    query : "(max-width:768px)"
+  });
+  
+  return <>{isMobile && children}</>
+}
+
+export const PC = ({children}) => {
+    const isPc = useMediaQuery({
+      query : "(min-width:769px)"
+    });
+    
+    return <>{isPc && children}</>
+  }
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
-      <Header />
-      <Main />
+      <Mobile>
+        Mobile
+      </Mobile>
+      <PC>
+        <Sidebar />
+        <Header />
+        <Main />
+      </PC>
     </div>
   );
 }
